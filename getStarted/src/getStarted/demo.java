985dab15fe1fb53a4c47913d70040fdac6dd432f
Paul Motter <pmotter@calpoly.edu>
2024-09-26 11:27:41 -0700

    First Commit to Cloned

--- getStarted/src/getStarted/demo.java
+++ getStarted/src/getStarted/demo.java
@@ -3,6 +3,6 @@ package getStarted;
 public class demo {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to CSC 305");
+		System.out.println("Welcome to CSC 305. I made a change.");
 	}
 }
